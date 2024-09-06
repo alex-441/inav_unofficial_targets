3438c7661d49036d7b9baaade58c187e5b7fa665
Alexander Khovansky <khovansky.alexander@gmail.com>
2024-09-06 16:01:57 +0300

    [Bugfix] Renamed ADC_CHANNEL<X>_PIN to ADC_CHANNEL_<X>_PIN

--- src/main/target/BETAFPVF435/target.h
+++ src/main/target/BETAFPVF435/target.h
@@ -172,9 +172,9 @@
 #define USE_ADC
 #define ADC_INSTANCE            ADC1
 #define ADC1_DMA_STREAM             DMA2_CHANNEL5
-#define ADC_CHANNEL1_PIN        PC2
-#define ADC_CHANNEL2_PIN        PC1
-//#define ADC_CHANNEL3_PIN        PC0
+#define ADC_CHANNEL_1_PIN        PC2
+#define ADC_CHANNEL_2_PIN        PC1
+//#define ADC_CHANNEL_3_PIN        PC0
 #define VBAT_ADC_CHANNEL            ADC_CHN_2
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_1
 //#define RSSI_ADC_CHANNEL        ADC_CHN_3
