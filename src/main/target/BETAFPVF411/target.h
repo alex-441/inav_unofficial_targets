35114d06edb6739b5cf73432ad0f08da3bd8e19d
Alexander Khovansky <khovansky.alexander@gmail.com>
2024-09-10 14:33:10 +0300

    Fixes 2024.09.10 - 1

--- src/main/target/BETAFPVF411/target.h
+++ src/main/target/BETAFPVF411/target.h
@@ -20,10 +20,10 @@
 #define TARGET_BOARD_IDENTIFIER "B411"
 #define USBD_PRODUCT_STRING     "BETAFPVF411"
 
-#define LED0                    PC13
+#define LED0                    PC14
 
-#define BEEPER                  PB2
-#define BEEPER_INVERTED
+//#define BEEPER                  PB2
+//#define BEEPER_INVERTED
 
 // *************** SPI **********************
 #define USE_SPI
@@ -38,10 +38,10 @@
 #define SPI2_MOSI_PIN  		   PB15
 
 // *************** SPI Gyro & ACC **********************
-#define USE_IMU_MPU6000
-#define MPU6000_CS_PIN          PA4
-#define MPU6000_SPI_BUS         BUS_SPI1
-#define IMU_MPU6000_ALIGN       CW180_DEG
+//#define USE_IMU_MPU6000
+//#define MPU6000_CS_PIN          PA4
+//#define MPU6000_SPI_BUS         BUS_SPI1
+//#define IMU_MPU6000_ALIGN       CW180_DEG
 
 #define USE_IMU_BMI270
 #define IMU_BMI270_ALIGN                CW90_DEG
@@ -52,12 +52,12 @@
 
 #define USE_I2C
 #define USE_I2C_DEVICE_1
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+#define I2C1_SCL                PC0
+#define I2C1_SDA                PC1
 
-#define USE_BARO
-#define BARO_I2C_BUS		    BUS_I2C1
-#define USE_BARO_ALL
+//#define USE_BARO
+//#define BARO_I2C_BUS		    BUS_I2C1
+//#define USE_BARO_ALL
 
 #define USE_MAG
 #define MAG_I2C_BUS             BUS_I2C1
@@ -69,11 +69,11 @@
 #define MAX7456_CS_PIN          PB12
 
 // *************** SPI FLASH **************************
-#define USE_FLASHFS
-#define USE_FLASH_M25P16
-#define M25P16_CS_PIN           PB2
-#define M25P16_SPI_BUS          BUS_SPI2
-#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+//#define USE_FLASHFS
+//#define USE_FLASH_M25P16
+//#define M25P16_CS_PIN           PB2
+//#define M25P16_SPI_BUS          BUS_SPI2
+//#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 
 // *************** UART *****************************
 #define USE_VCP
@@ -87,24 +87,24 @@
 #define UART2_RX_PIN            PA3
 
 #define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN     PB3
-#define SOFTSERIAL_1_RX_PIN     PB3
+#define SOFTSERIAL_1_TX_PIN     PA8
+#define SOFTSERIAL_1_RX_PIN     PA8
 
 #define USE_SOFTSERIAL2
-#define SOFTSERIAL_2_TX_PIN     PB10
-#define SOFTSERIAL_2_RX_PIN     PB10
+#define SOFTSERIAL_2_TX_PIN     PB1
+#define SOFTSERIAL_2_RX_PIN     PB1
 
 #define SERIAL_PORT_COUNT       5
 
-#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
-#define SERIALRX_PROVIDER       SERIALRX_CRSF
-#define SERIALRX_UART           SERIAL_PORT_USART1
+//#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
+//#define SERIALRX_PROVIDER       SERIALRX_CRSF
+//#define SERIALRX_UART           SERIAL_PORT_USART1
 
 // *************** ADC *****************************
 #define USE_ADC
 #define ADC_INSTANCE                    ADC1
-#define ADC_CHANNEL_1_PIN               PB0
-#define ADC_CHANNEL_2_PIN               PB1
+#define ADC_CHANNEL_1_PIN               PA1
+#define ADC_CHANNEL_2_PIN               PB0
 
 #define VBAT_ADC_CHANNEL                ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL       ADC_CHN_2
@@ -112,11 +112,11 @@
 #define VBAT_SCALE_DEFAULT              1100
 
 // *************** LED2812 ************************
-#define USE_LED_STRIP
-#define WS2811_PIN                      PA8
+//#define USE_LED_STRIP
+//#define WS2811_PIN                      PA8
 
 // ***************  OTHERS *************************
-#define DEFAULT_FEATURES                (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_VBAT | FEATURE_TELEMETRY | FEATURE_BLACKBOX)
+#define DEFAULT_FEATURES                (FEATURE_OSD | FEATURE_VBAT | FEATURE_TELEMETRY | FEATURE_CURRENT_METER)
 
 #define USE_DSHOT
 #define USE_ESC_SENSOR
